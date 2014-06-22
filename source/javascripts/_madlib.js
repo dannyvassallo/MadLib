da8980a0f8b9f922bd213b62c1e8e73196f57117
Dan Vassallo <danielvassallo87@gmail.com>
2014-06-22 13:20:16 -0400

    Tinkering with previous/next. Code is horrible.

--- source/javascripts/_madlib.js
+++ source/javascripts/_madlib.js
@@ -34,6 +34,27 @@ $(function() {
 });
 
 
-$(document).ready(function(){
-  $(".form-group").hide();
+
+
+
+
+
+
+$('#next').click(function() {
+    $('.current').removeClass('current').hide()
+        .next().show().addClass('current');
+    if ($('.current').hasClass('last')) {
+        $('#next').attr('disabled', true);
+    }
+    $('#prev').attr('disabled', null);
 });
+
+$('#prev').click(function() {
+    $('.current').removeClass('current').hide()
+        .prev().show().addClass('current');
+    if ($('.current').hasClass('first')) {
+        $('#prev').attr('disabled', true);
+    }
+    $('#next').attr('disabled', null);
+});
+
